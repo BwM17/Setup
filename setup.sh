b5bb8578b5f7c4884987346c0ee1c11d8ea3aadc
Berny <89018551+BwM17@users.noreply.github.com>
2025-02-10 09:13:58 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash 
+#chmod +x 
+#sudo ./setup.sh
+sudo dnf -y install dnf-plugins-core
+sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+
+sudo dnf update && sudo dnf upgrade
+
+sudo systemctl enable --now docker 
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.21.5 
+
+firefox https://localhost:9443 
+
+
+#Pre installation 
+
+#sudo groupadd docker
+#sudo usermode -aG docker $USER
